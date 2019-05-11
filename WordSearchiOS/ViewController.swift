52bd3855383c20a5ca0ad7f02849ca039de4629f
anmolbajajnet <hello@anmolbajaj.net>
2019-05-11 13:27:09 -0700

    UICollectionView displays words from an array data source

--- WordSearchiOS/ViewController.swift
+++ WordSearchiOS/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let words = ["A","B","C","D","E","F","G","H","I","G","H"]
+    
+    @IBOutlet weak var wordCollectionView: UICollectionView!
+    
+    let cellIdentifier = "WordCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionViewSetup()
     }
+    
+    //Set up data source methods
+    private func collectionViewSetup() {
+        wordCollectionView.delegate = self
+        wordCollectionView.dataSource = self
+    }
+ 
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = wordCollectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! WordCollectionViewCell
+        cell.wordLabel.text = words[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+    
 
+    
 }
 
---
+++ WordSearchiOS/WordCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WordCollectionViewCell.swift
+//  WordSearchiOS
+//
+//  Created by Anmol Bajaj on 2019-05-11.
+//  Copyright © 2019 Anmol Bajaj. All rights reserved.
+//
+
+import UIKit
+
+class WordCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var wordLabel: UILabel!
+    
+}
